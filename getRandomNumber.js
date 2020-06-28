7a82ca16b9030e872edd6678232dc2c772753b7b
aramtantushyan <60556457+aramtantushyan@users.noreply.github.com>
2020-06-28 14:57:58 +0400

    added examples of usage

--- getRandomNumber.js
+++ getRandomNumber.js
@@ -3,3 +3,19 @@ function getRandomLetter () {
     let num = Math.round(Math.random() * characters.length);
     return characters[num];
 }
+
+function choosePuppyNamebyThreeRandomLetter () {
+    let randomLetters = [];
+    for (let i = 0; i < 3; i++) {
+        randomLetters.push(getRandomLetter());
+    }
+    return randomLetters.toString();
+}
+
+function generatePassword () {
+    let password = '';
+    for (let i = 0; i < 10; i ++){
+        password += getRandomLetter();
+    }
+    return password;
+}
