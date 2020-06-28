5a425a9746c40b0a552bbc17c70a417ecc83b6a8
aramtantushyan <60556457+aramtantushyan@users.noreply.github.com>
2020-06-28 15:03:22 +0400

    added an example of function usage

--- getRandomLetter.js
+++ getRandomLetter.js
@@ -1,3 +1,12 @@
 function getRandomNumber () {
     return Math.random();
 }
+
+function generateBinaryNumbers () {
+    let id = '';
+    for (let i = 0; i < 3; i++) {
+        id += Math.round(getRandomNumber());
+    }
+    return id;
+}
+
